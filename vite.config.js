bf5f4a7a7dc09b2b62bdc4f8048cc4e786d1cf9c
Ali Shah <s.ashah@outlook.com>
2021-09-13 23:37:37 -0400

    [Added] deployment path to `vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://shah-a.github.io/few2.3-lab-03-pizza-order/',
   plugins: [reactRefresh()]
 })
